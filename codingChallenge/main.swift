01a7ec62e8ddbb7ead01c45f8de59e2ecad85f3c
Alan Holguin <alanjholguin@gmail.com>
2018-06-11 00:07:23 -0400

    Comments in Main

--- codingChallenge/main.swift
+++ codingChallenge/main.swift
@@ -1,5 +1,9 @@
 import Foundation
+// Initialize Game Brain Class to power entire program
 let gameBrain = GameBrain()
+// Run User Set Up To Select Name and Class
 gameBrain.userSetUp()
+// Run game
 gameBrain.enterTower()
+// Player Wins
 gameBrain.playerBeatsTower()
